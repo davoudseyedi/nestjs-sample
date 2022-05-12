e3ba813e4f91ec5ed05db6d7e8ac9d08d1918d42
davoudseyedi <davoudseyedi@gmail.com>
2022-05-12 21:53:41 +0430

    added docker-compose postgres

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,14 @@
 import {Injectable} from "@nestjs/common";
 
 @Injectable({})
-export class AuthService{}
+export class AuthService{
+
+    login(){
+        return {msg:'I am signin in'}
+    }
+
+    signup(){
+        return {msg:'I am signed up'}
+    }
+}
+
